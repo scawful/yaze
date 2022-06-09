accf24c2442bd18383e7f967dcb229982c7ae075
scawful <justin.scofield@outlook.com>
2022-06-08 23:05:17 -0400

    Added yaze header and source

---
+++ src/yaze.cc
@@ -0,0 +1,1 @@
+#include "yaze.h"
---
+++ src/yaze.h
@@ -0,0 +1,20 @@
+#ifndef YAZE_YAZE_H
+#define YAZE_YAZE_H
+
+#include <iostream>
+#include <memory>
+#include <utility>
+#include <algorithm>
+#include <functional>
+
+#include <string>
+#include <sstream>
+#include <array>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+
+#include "Application/Core/Controller.h"
+#include "Application/Core/EntryPoint.h"
+
+#endif  // YAZE_YAZE_H
