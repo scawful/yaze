7cb3353736c0ae4e27bae19f6467f8fe1491c923
Justin Scofield <justin.scofield@outlook.com>
2022-06-10 13:07:15 -0400

    Adding OpenGL support to the Bitmap object

--- src/Application/Graphics/Bitmap.h
+++ src/Application/Graphics/Bitmap.h
@@ -1,14 +1,22 @@
 #ifndef YAZE_APPLICATION_UTILS_BITMAP_H
 #define YAZE_APPLICATION_UTILS_BITMAP_H
 
+#include "GL/glew.h"
+#include <SDL2/SDL_opengl.h>
+
+#include "Utils/ROM.h"
+
 namespace yaze {
 namespace Application {
-namespace Utils {
+namespace Graphics {
 class Bitmap {
+ public:
+  Bitmap();
 
-  
+  bool LoadBitmapFromROM(unsigned char* texture_data, GLuint* out_texture,
+                         int* out_width, int* out_height);
 };
-}  // namespace Utils
+}  // namespace Graphics
 }  // namespace Application
 }  // namespace yaze
 
