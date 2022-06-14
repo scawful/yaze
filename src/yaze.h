c6904fb0c7ecbe6b93b176ef359eee20ce2b7b65
scawful <justin.scofield@outlook.com>
2022-06-13 22:17:44 -0400

    cleaning up the mess i've made

--- src/yaze.h
+++ src/yaze.h
@@ -1,19 +1,6 @@
 #ifndef YAZE_YAZE_H
 #define YAZE_YAZE_H
 
-#include <iostream>
-#include <memory>
-#include <utility>
-#include <algorithm>
-#include <functional>
-
-#include <string>
-#include <sstream>
-#include <array>
-#include <vector>
-#include <unordered_map>
-#include <unordered_set>
-
 #include "Application/Core/controller.h"
 #include "Application/Core/entry_point.h"
 
