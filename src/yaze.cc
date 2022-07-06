7cde414ba77b74acb463d064f8a542a6e230a7c4
scawful <justin.scofield@outlook.com>
2022-07-06 19:17:26 -0400

    remove entry_point header

--- src/yaze.cc
+++ src/yaze.cc
@@ -1,1 +1,13 @@
 #include "yaze.h"
+
+int main(int argc, char** argv) {
+  yaze::app::core::Controller controller;
+  controller.onEntry();
+  while (controller.isActive()) {
+    controller.onInput();
+    controller.onLoad();
+    controller.doRender();
+  }
+  controller.onExit();
+  return EXIT_SUCCESS;
+}
