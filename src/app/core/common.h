78ae6f28f5efadb6fe2d2a0517a0045f8ca6fa69
Justin Scofield <justin.scofield@outlook.com>
2022-07-12 21:33:19 -0400

    formatting

--- src/app/core/common.h
+++ src/app/core/common.h
@@ -11,11 +11,6 @@ unsigned int SnesToPc(unsigned int addr);
 int AddressFromBytes(uint8_t addr1, uint8_t addr2, uint8_t addr3);
 int HexToDec(char *input, int length);
 
-template<typename T>
-T* ReserveBytes(size_t size) {
-  auto bytes = new T[size];
-}
-
 }  // namespace core
 }  // namespace app
 }  // namespace yaze
