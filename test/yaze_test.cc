f0316d9f93781625aafa7e2540110a6517ac3dff
scawful <justin.scofield@outlook.com>
2022-07-12 23:22:47 -0400

    remove unused test

--- test/yaze_test.cc
+++ test/yaze_test.cc
@@ -1,15 +1,6 @@
 #define SDL_MAIN_HANDLED
 #include <gtest/gtest.h>
 
-namespace YazeTests {
-
-TEST(YazeapplicationTests, TemplateTest) {
-  int i = 0;
-  ASSERT_EQ(i, 0);
-}
-
-}  // namespace YazeTests
-
 int main(int argc, char* argv[]) {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
