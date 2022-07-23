979150afcff4e85bb3ea4350437288d6db67eb45
Justin Scofield <justin.scofield@outlook.com>
2022-07-23 18:43:35 -0400

    Added failure signal handler

--- src/yaze.cc
+++ src/yaze.cc
@@ -1,6 +1,13 @@
+#include "absl/debugging/failure_signal_handler.h"
+#include "absl/debugging/symbolize.h"
 #include "app/core/controller.h"
 
 int main(int argc, char** argv) {
+  absl::InitializeSymbolizer(argv[0]);
+
+  absl::FailureSignalHandlerOptions options;
+  absl::InstallFailureSignalHandler(options);
+
   yaze::app::core::Controller controller;
   controller.onEntry();
   while (controller.isActive()) {
