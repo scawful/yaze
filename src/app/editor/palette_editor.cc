8fa88bd4a764275d783ce33112e33de7f65f08a9
Justin Scofield <justin.scofield@outlook.com>
2022-07-25 11:27:30 -0400

    Added PaletteEditor class

---
+++ src/app/editor/palette_editor.cc
@@ -0,0 +1,17 @@
+#include "palette_editor.h"
+
+#include <imgui/imgui.h>
+
+#include "app/gfx/snes_palette.h"
+#include "gui/canvas.h"
+#include "gui/icons.h"
+
+namespace yaze {
+namespace app {
+namespace editor {
+
+void PaletteEditor::Update() {}
+
+}  // namespace editor
+}  // namespace app
+}  // namespace yaze
