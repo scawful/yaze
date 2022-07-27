74b1b86997cfe699c1f3466941834c0a211f9b59
Justin Scofield <justin.scofield@outlook.com>
2022-07-27 21:26:19 +0000

    Added failure signal handler to yaze_test

--- test/yaze_test.cc
+++ test/yaze_test.cc
@@ -1,7 +1,14 @@
 #define SDL_MAIN_HANDLED
 #include <gtest/gtest.h>
 
+#include "absl/debugging/failure_signal_handler.h"
+#include "absl/debugging/symbolize.h"
+
 int main(int argc, char* argv[]) {
+  absl::InitializeSymbolizer(argv[0]);
+
+  absl::FailureSignalHandlerOptions options;
+  absl::InstallFailureSignalHandler(options);
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
