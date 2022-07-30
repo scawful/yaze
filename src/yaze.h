39c6683ff8b560b4c7cf4bafa6cbeec946c9b23b
Justin Scofield <47263509+scawful@users.noreply.github.com>
2022-07-30 13:31:03 -0400

    preprocessor macro, cmake conditional for SDL_main

--- src/yaze.h
+++ src/yaze.h
@@ -1,6 +1,10 @@
 #ifndef YAZE_H
 #define YAZE_H
 
+#if defined(_WIN32)
+#define main SDL_main
+#endif
+
 #include "absl/debugging/failure_signal_handler.h"
 #include "absl/debugging/symbolize.h"
 #include "app/core/controller.h"
