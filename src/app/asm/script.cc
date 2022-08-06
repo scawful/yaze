b0370b5c0dc2e686458971747351e884769215fe
Justin Scofield <47263509+scawful@users.noreply.github.com>
2022-08-06 13:19:24 -0400

    Improve Script and added ApplyPatchToROM with Asar

--- src/app/asm/script.cc
+++ src/app/asm/script.cc
@@ -1,18 +1,27 @@
 #include "script.h"
 
-#include <asar.h>
+#include <interface-lib.h>
 
 #include <array>
 #include <fstream>
 #include <sstream>
 
+#include "absl/status/status.h"
 #include "absl/status/statusor.h"
 #include "absl/strings/string_view.h"
 #include "app/core/constants.h"
+#include "app/rom.h"
 
 namespace yaze {
 namespace app {
 namespace snes_asm {
+
+absl::Status Script::ApplyPatchToROM(ROM& rom) {
+  if (!asar_patch(patch_filename_, rom_.data(), patch_size_, rom_.size())) {
+    return absl::InternalError("Unable to apply patch");
+  }
+  return absl::OkStatus();
+}
 
 absl::StatusOr<absl::string_view> Script::GenerateMosaicChangeAssembly(
     std::array<int, core::kNumOverworldMaps> mosaic_tiles) {
@@ -23,8 +32,8 @@ absl::StatusOr<absl::string_view> Script::GenerateMosaicChangeAssembly(
         "Couldn't open mosaic change template file");
   }
   std::stringstream assembly;
-
-  assembly << absl::StrCat("org ", kDefaultMosaicHook);
+  assembly << "org ";
+  assembly << kDefaultMosaicHook;
   assembly << file.rdbuf();
 
   file.close();
