cde58f9d82eced45bcf7a23e28bdc9dbc377ac85
Justin Scofield <47263509+scawful@users.noreply.github.com>
2022-08-06 13:19:24 -0400

    Improve Script and added ApplyPatchToROM with Asar

--- src/app/asm/script.h
+++ src/app/asm/script.h
@@ -1,15 +1,17 @@
 #ifndef YAZE_APP_ASM_SCRIPT_H
 #define YAZE_APP_ASM_SCRIPT_H
 
-#include <asar.h>
+#include <interface-lib.h>
 
 #include <array>
 #include <fstream>
 #include <sstream>
 
+#include "absl/status/status.h"
 #include "absl/status/statusor.h"
 #include "absl/strings/string_view.h"
 #include "app/core/constants.h"
+#include "app/rom.h"
 
 namespace yaze {
 namespace app {
@@ -19,8 +21,17 @@ constexpr char kDefaultMosaicHook[] = "$02AADB";
 
 class Script {
  public:
+  Script() = default;
+
+  absl::Status ApplyPatchToROM(ROM& rom);
+
   absl::StatusOr<absl::string_view> GenerateMosaicChangeAssembly(
       std::array<int, core::kNumOverworldMaps> mosaic_tiles);
+
+ private:
+  int64_t patch_size_;
+  std::string patch_filename_;
+  std::string patch_contents_;
 };
 
 }  // namespace snes_asm
