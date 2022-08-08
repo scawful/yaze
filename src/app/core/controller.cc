fb4dc48add6939a9eb63c177f55c169f31154648
Justin Scofield <47263509+scawful@users.noreply.github.com>
2022-08-08 08:31:02 -0400

    chore: make window dimensions a constant

--- src/app/core/controller.cc
+++ src/app/core/controller.cc
@@ -154,8 +154,8 @@ absl::Status Controller::CreateWindow() {
         SDL_CreateWindow("Yet Another Zelda3 Editor",  // window title
                          SDL_WINDOWPOS_UNDEFINED,      // initial x position
                          SDL_WINDOWPOS_UNDEFINED,      // initial y position
-                         1200,                         // width, in pixels
-                         800,                          // height, in pixels
+                         kScreenWidth,                 // width, in pixels
+                         kScreenHeight,                // height, in pixels
                          SDL_WINDOW_RESIZABLE | SDL_WINDOW_OPENGL),
         sdl_deleter());
     if (window_ == nullptr) {
