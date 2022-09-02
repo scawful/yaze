970b0829262df17968529fcf87338a06a0bf09dc
scawful <justin.scofield@outlook.com>
2022-09-02 17:42:09 -0500

    housekeeping

--- src/app/gfx/bitmap.h
+++ src/app/gfx/bitmap.h
@@ -46,12 +46,8 @@ class Bitmap {
 
  private:
   struct sdl_deleter {
-    void operator()(SDL_Texture *p) const {
-      SDL_DestroyTexture(p);
-    }
-    void operator()(SDL_Surface *p) const {
-      SDL_FreeSurface(p);
-    }
+    void operator()(SDL_Texture *p) const { SDL_DestroyTexture(p); }
+    void operator()(SDL_Surface *p) const { SDL_FreeSurface(p); }
   };
 
   int width_ = 0;
