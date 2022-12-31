b83758a944ed8d357085249317692cead2d3bdb9
scawful <justin.scofield@outlook.com>
2022-12-30 19:08:27 -0600

    add left item label api override

--- src/gui/input.h
+++ src/gui/input.h
@@ -7,11 +7,21 @@
 #include <cstddef>
 #include <cstdint>
 
+#include "absl/strings/string_view.h"
+
 namespace yaze {
 namespace gui {
 
 IMGUI_API bool InputHex(const char* label, int* data);
 IMGUI_API bool InputHexShort(const char* label, int* data);
+
+using ItemLabelFlags = enum ItemLabelFlag {
+  Left = 1u << 0u,
+  Right = 1u << 1u,
+  Default = Left,
+};
+
+IMGUI_API void ItemLabel(absl::string_view title, ItemLabelFlags flags);
 
 }  // namespace gui
 }  // namespace yaze
