2d3937e7919bc59c6dcbc61d10ce7b5c5c0506dd
scawful <justin.scofield@outlook.com>
2023-01-01 16:11:44 -0600

    added snes_spc library

--- src/app/editor/music_editor.cc
+++ src/app/editor/music_editor.cc
@@ -1,12 +1,67 @@
 #include "music_editor.h"
 
+#include <imgui/imgui.h>
+
+#include "absl/strings/str_format.h"
+#include "app/editor/assembly_editor.h"
+#include "gui/canvas.h"
 #include "gui/icons.h"
 #include "gui/input.h"
-#include "imgui.h"
+#include "snes_spc/demo/demo_util.h"
+#include "snes_spc/demo/wave_writer.h"
+#include "snes_spc/snes_spc/spc.h"
 
 namespace yaze {
 namespace app {
 namespace editor {
+
+namespace {
+
+void PlaySPC() {
+  /* Create emulator and filter */
+  SNES_SPC* snes_spc = spc_new();
+  SPC_Filter* filter = spc_filter_new();
+  if (!snes_spc || !filter) error("Out of memory");
+
+  /* Load SPC */
+  {
+    /* Load file into memory */
+    long spc_size;
+    void* spc = load_file("assets/music/hyrule_field.spc", &spc_size);
+
+    /* Load SPC data into emulator */
+    error(spc_load_spc(snes_spc, spc, spc_size));
+    free(spc); /* emulator makes copy of data */
+
+    /* Most SPC files have garbage data in the echo buffer, so clear that */
+    spc_clear_echo(snes_spc);
+
+    /* Clear filter before playing */
+    spc_filter_clear(filter);
+  }
+
+  /* Record 20 seconds to wave file */
+  wave_open(spc_sample_rate, "out.wav");
+  wave_enable_stereo();
+  while (wave_sample_count() < 20 * spc_sample_rate * 2) {
+/* Play into buffer */
+#define BUF_SIZE 2048
+    short buf[BUF_SIZE];
+    error(spc_play(snes_spc, BUF_SIZE, buf));
+
+    /* Filter samples */
+    spc_filter_run(filter, buf, BUF_SIZE);
+
+    wave_write(buf, BUF_SIZE);
+  }
+
+  /* Cleanup */
+  spc_filter_delete(filter);
+  spc_delete(snes_spc);
+  wave_close();
+}
+
+}  // namespace
 
 void MusicEditor::Update() {
   if (ImGui::BeginTable("MusicEditorColumns", 2, music_editor_flags_,
@@ -162,6 +217,12 @@ void MusicEditor::DrawToolset() {
   static int selected_option = 0;
   static int current_volume = 0;
   const int MAX_VOLUME = 100;
+
+
+  if (is_playing) {
+    PlaySPC();
+  }
+
   gui::ItemLabel("Select a song to edit: ", gui::ItemLabelFlags::Left);
   ImGui::Combo("#songs_in_game", &selected_option, kGameSongs, 30);
 
