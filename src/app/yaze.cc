004e5917ee02c7c7060c5e7d8e30faf7728515dc
scawful <justin.scofield@outlook.com>
2023-11-24 13:35:34 -0500

    Add Cocoa and macOS code to CMakeLists

--- src/app/yaze.cc
+++ src/app/yaze.cc
@@ -1,5 +1,7 @@
 #if defined(_WIN32)
 #define main SDL_main
+#elif __APPLE__
+#include "app/core/platform/app_delegate.h"
 #endif
 
 #include "absl/debugging/failure_signal_handler.h"
@@ -17,6 +19,11 @@ int main(int argc, char** argv) {
   yaze::app::core::Controller controller;
 
   EXIT_IF_ERROR(controller.OnEntry())
+
+#ifdef __APPLE__
+  InitializeCocoa();
+#endif
+
   while (controller.IsActive()) {
     controller.OnInput();
     controller.OnLoad();
