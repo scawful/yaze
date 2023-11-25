82a8e42d34e477c0161a2e324161710ed3eef99e
Justin Scofield <47263509+scawful@users.noreply.github.com>
2023-11-24 22:11:44 -0500

    Add Windows font loading from system fonts

--- src/app/core/platform/font_loader.h
+++ src/app/core/platform/font_loader.h
@@ -5,4 +5,11 @@
 // Function declaration for loading system fonts into ImGui
 void LoadSystemFonts();
 
-#endif // FONTLOADER_H
+#ifdef _WIN32
+#include <Windows.h>
+// Windows specific function declaration for loading system fonts into ImGui
+int CALLBACK EnumFontFamExProc(const LOGFONT* lpelfe, const TEXTMETRIC* lpntme,
+                               DWORD FontType, LPARAM lParam);
+#endif
+
+#endif  // FONTLOADER_H
