4d05f953126949afb557d6a3ef32b0ecd2dd29ab
scawful <justin.scofield@outlook.com>
2023-12-19 18:52:13 -0500

    Add clipboard fn headers per platform

--- src/app/core/platform/clipboard.h
+++ src/app/core/platform/clipboard.h
@@ -1,4 +1,10 @@
+#ifndef YAZE_APP_CORE_PLATFORM_CLIPBOARD_H
+#define YAZE_APP_CORE_PLATFORM_CLIPBOARD_H
+
 #ifdef _WIN32
+
+void CopyImageToClipboard(const std::vector<uint8_t>& data);
+void GetImageFromClipboard(std::vector<uint8_t>& data, int& width, int& height);
 
 #elif defined(__APPLE__)
 
@@ -7,4 +13,20 @@
 void CopyImageToClipboard(const std::vector<uint8_t>& data);
 void GetImageFromClipboard(std::vector<uint8_t>& data, int& width, int& height);
 
+#elif defined(__linux__)
+
+#include <vector>
+
+void CopyImageToClipboard(const std::vector<uint8_t>& data) {
+  std::cout << "CopyImageToClipboard() is not implemented on Linux."
+            << std::endl;
+}
+void GetImageFromClipboard(std::vector<uint8_t>& data, int& width,
+                           int& height) {
+  std::cout << "GetImageFromClipboard() is not implemented on Linux."
+            << std::endl;
+}
+
 #endif
+
+#endif  // YAZE_APP_CORE_PLATFORM_CLIPBOARD_H
