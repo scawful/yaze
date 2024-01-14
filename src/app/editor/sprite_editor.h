54a6527b7d7622da08f1a372de3d4c8801b997fd
scawful <justin.scofield@outlook.com>
2024-01-14 11:15:48 -0500

    SpriteEditor skeleton

--- src/app/editor/sprite_editor.h
+++ src/app/editor/sprite_editor.h
@@ -2,14 +2,24 @@
 #define YAZE_APP_EDITOR_SPRITE_EDITOR_H
 
 #include "absl/status/status.h"
+#include "app/gui/canvas.h"
+#include "app/rom.h"
 
 namespace yaze {
 namespace app {
 namespace editor {
 
-class SpriteEditor {
-  public:
-    absl::Status Update();
+class SpriteEditor : public SharedROM {
+ public:
+  absl::Status Update();
+
+ private:
+  void DrawEditorTable();
+  void DrawSpriteCanvas();
+  void DrawCurrentSheets();
+
+  uint8_t current_sheets_[8];
+  bool sheets_loaded_ = false;
 };
 
 }  // namespace editor
