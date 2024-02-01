8e32ae63a0dbae3eed768962afd07ee1bbacf417
scawful <justin.scofield@outlook.com>
2024-02-01 00:08:18 -0500

    Add clipboard.cc for linux (unimplemented)

---
+++ src/app/core/platform/clipboard.cc
@@ -0,0 +1,5 @@
+#include "app/core/platform/clipboard.h"
+
+void CopyImageToClipboard(const std::vector<uint8_t>& data) {}
+void GetImageFromClipboard(std::vector<uint8_t>& data, int& width,
+                           int& height) {}
