88207094ce74e08533c2c5730490ce641fc2e461
scawful <justin.scofield@outlook.com>
2024-07-13 10:23:00 -0400

    add sprite canvas, sprite sheets to sprite editor

--- src/app/editor/sprite_editor.cc
+++ src/app/editor/sprite_editor.cc
@@ -1,5 +1,7 @@
 #include "sprite_editor.h"
 
+#include <gui/input.h>
+
 namespace yaze {
 namespace app {
 namespace editor {
@@ -15,52 +17,101 @@ using ImGui::Text;
 absl::Status SpriteEditor::Update() {
   if (rom()->is_loaded() && !sheets_loaded_) {
     // Load the values for current_sheets_ array
-
     sheets_loaded_ = true;
   }
 
-  // if (ImGui::BeginTable({"Canvas", "Graphics"}, 2, nullptr, ImVec2(0, 0))) {
-  //   TableSetupColumn("Canvas", ImGuiTableColumnFlags_WidthStretch,
-  //                    ImGui::GetContentRegionAvail().x);
-  //   TableSetupColumn("Tile Selector", ImGuiTableColumnFlags_WidthFixed, 256);
-  //   TableHeadersRow();
-  //   TableNextRow();
-  //   TableNextColumn();
-  //   DrawSpriteCanvas();
+  if (ImGui::BeginTable("##SpriteCanvasTable", 2, ImGuiTableFlags_Resizable,
+                        ImVec2(0, 0))) {
+    TableSetupColumn("Canvas", ImGuiTableColumnFlags_WidthStretch,
+                     ImGui::GetContentRegionAvail().x);
+    TableSetupColumn("Tile Selector", ImGuiTableColumnFlags_WidthFixed, 256);
+    TableHeadersRow();
+    TableNextRow();
+    TableNextColumn();
+    DrawSpriteCanvas();
 
-  //   TableNextColumn();
-  //   if (sheets_loaded_) {
-  //     DrawCurrentSheets();
-  //   }
-    
-  //   ImGui::EndTable();
-  // }
+    TableNextColumn();
+    if (sheets_loaded_) {
+      DrawCurrentSheets();
+    }
+    ImGui::EndTable();
+  }
 
   return absl::OkStatus();
 }
 
-void SpriteEditor::DrawEditorTable() {}
+void SpriteEditor::DrawSpriteCanvas() {
+  if (ImGui::BeginChild(gui::GetID("##SpriteCanvas"),
+                        ImGui::GetContentRegionAvail(), true,
+                        ImGuiWindowFlags_AlwaysVerticalScrollbar |
+                            ImGuiWindowFlags_AlwaysHorizontalScrollbar)) {
+    sprite_canvas_.DrawBackground(ImVec2(0x200, 0x200));
+    sprite_canvas_.DrawContextMenu();
+    // sprite_canvas_.DrawBitmap(oam_bitmap_, 2, 2);
+    sprite_canvas_.DrawGrid(8.0f);
+    sprite_canvas_.DrawOverlay();
 
-void SpriteEditor::DrawSpriteCanvas() {}
+    // Draw a table with OAM configuration
+    // X, Y, Tile, Palette, Priority, Flip X, Flip Y
+    if (ImGui::BeginTable("##OAMTable", 7, ImGuiTableFlags_None,
+                          ImVec2(0, 0))) {
+      TableSetupColumn("X", ImGuiTableColumnFlags_WidthStretch);
+      TableSetupColumn("Y", ImGuiTableColumnFlags_WidthStretch);
+      TableSetupColumn("Tile", ImGuiTableColumnFlags_WidthStretch);
+      TableSetupColumn("Palette", ImGuiTableColumnFlags_WidthStretch);
+      TableSetupColumn("Priority", ImGuiTableColumnFlags_WidthStretch);
+      TableSetupColumn("Flip X", ImGuiTableColumnFlags_WidthStretch);
+      TableSetupColumn("Flip Y", ImGuiTableColumnFlags_WidthStretch);
+      TableHeadersRow();
+      TableNextRow();
+
+      TableNextColumn();
+      gui::InputHexWord("", &oam_config_.x);
+
+      TableNextColumn();
+      gui::InputHexWord("", &oam_config_.y);
+
+      TableNextColumn();
+      gui::InputHexByte("", &oam_config_.tile);
+
+      TableNextColumn();
+      gui::InputHexByte("", &oam_config_.palette);
+
+      TableNextColumn();
+      gui::InputHexByte("", &oam_config_.priority);
+
+      TableNextColumn();
+      ImGui::Checkbox("", &oam_config_.flip_x);
+
+      TableNextColumn();
+      ImGui::Checkbox("", &oam_config_.flip_y);
+
+      ImGui::EndTable();
+    }
+
+    ImGui::EndChild();
+  }
+}
 
 void SpriteEditor::DrawCurrentSheets() {
-  static gui::Canvas graphics_sheet_canvas;
   for (int i = 0; i < 8; i++) {
-    ImGui::PushStyleVar(ImGuiStyleVar_FramePadding, ImVec2(0, 0));
-    ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0, 0));
-    if (ImGuiID child_id = ImGui::GetID((void *)(intptr_t)7);
-        ImGui::BeginChild(child_id, ImGui::GetContentRegionAvail(), true,
-                          ImGuiWindowFlags_AlwaysVerticalScrollbar |
-                              ImGuiWindowFlags_AlwaysHorizontalScrollbar)) {
-      graphics_sheet_canvas.DrawBackground(ImVec2(0x200 * 8, 0x200 * 8));
-      ImGui::PopStyleVar(2);
+    // ImGui::PushStyleVar(ImGuiStyleVar_FramePadding, ImVec2(0, 0));
+    // ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0, 0));
+    // std::string sheet_label = absl::StrFormat("Sheet %d", i);
+    // gui::InputHexByte(sheet_label.c_str(), &current_sheets_[i]);
+    if (ImGui::BeginChild(gui::GetID("sheet_label"),
+                          ImVec2(ImGui::GetContentRegionAvail().x, 0), true,
+                          ImGuiWindowFlags_NoDecoration)) {
+      static gui::Canvas graphics_sheet_canvas;
+      graphics_sheet_canvas.DrawBackground(ImVec2(0x80 * 2, 0x20 * 2));
+      // ImGui::PopStyleVar(2);
       graphics_sheet_canvas.DrawContextMenu();
       graphics_sheet_canvas.DrawBitmap(
-          *rom()->bitmap_manager()[current_sheets_[i]], 2, 2);
+          *rom()->bitmap_manager()[current_sheets_[i]], 2, 2, 2);
       graphics_sheet_canvas.DrawGrid(64.0f);
       graphics_sheet_canvas.DrawOverlay();
+      ImGui::EndChild();
     }
-    ImGui::EndChild();
   }
 }
 
