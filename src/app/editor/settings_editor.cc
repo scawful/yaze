53604dc2401b4a93b3e8e5767d5cb78ce6ffca60
scawful <justin.scofield@outlook.com>
2024-07-20 10:31:22 -0400

    update FlagsMenu

--- src/app/editor/settings_editor.cc
+++ src/app/editor/settings_editor.cc
@@ -8,8 +8,13 @@ namespace yaze {
 namespace app {
 namespace editor {
 
+using ImGui::BeginChild;
+using ImGui::BeginMenu;
 using ImGui::BeginTabBar;
 using ImGui::BeginTabItem;
+using ImGui::Checkbox;
+using ImGui::EndChild;
+using ImGui::EndMenu;
 using ImGui::EndTabBar;
 using ImGui::EndTabItem;
 using ImGui::Text;
@@ -17,8 +22,12 @@ using ImGui::Text;
 absl::Status SettingsEditor::Update() {
   if (BeginTabBar("Settings", ImGuiTabBarFlags_None)) {
     if (BeginTabItem("General")) {
-      static FlagsMenu flags;
-      flags.Draw();
+      if (BeginChild("##GeneralSettingsStyleWrapper", ImVec2(0, 0),
+                     ImGuiChildFlags_FrameStyle)) {
+        static FlagsMenu flags;
+        flags.Draw();
+      }
+      EndChild();
       EndTabItem();
     }
     if (BeginTabItem("Keyboard Shortcuts")) {
--- src/app/editor/utils/flags.h
+++ src/app/editor/utils/flags.h
@@ -16,50 +16,47 @@ using ImGui::Separator;
 
 struct FlagsMenu : public core::ExperimentFlags {
   void Draw() {
-    if (BeginMenu("Experiment Flags")) {
-      if (BeginMenu("Overworld Flags")) {
-        Checkbox("Enable Overworld Sprites",
-                 &mutable_flags()->overworld.kDrawOverworldSprites);
-        Separator();
-        Checkbox("Save Overworld Maps",
-                 &mutable_flags()->overworld.kSaveOverworldMaps);
-        Checkbox("Save Overworld Entrances",
-                 &mutable_flags()->overworld.kSaveOverworldEntrances);
-        Checkbox("Save Overworld Exits",
-                 &mutable_flags()->overworld.kSaveOverworldExits);
-        Checkbox("Save Overworld Items",
-                 &mutable_flags()->overworld.kSaveOverworldItems);
-        Checkbox("Save Overworld Properties",
-                 &mutable_flags()->overworld.kSaveOverworldProperties);
-        ImGui::EndMenu();
-      }
-
-      if (BeginMenu("Dungeon Flags")) {
-        Checkbox("Draw Dungeon Room Graphics",
-                 &mutable_flags()->kDrawDungeonRoomGraphics);
-        Separator();
-        Checkbox("Save Dungeon Maps", &mutable_flags()->kSaveDungeonMaps);
-        ImGui::EndMenu();
-      }
-
-      if (BeginMenu("Emulator Flags")) {
-        Checkbox("Load Audio Device", &mutable_flags()->kLoadAudioDevice);
-        ImGui::EndMenu();
-      }
-
-      Checkbox("Use built-in file dialog",
-               &mutable_flags()->kNewFileDialogWrapper);
-      Checkbox("Enable Console Logging", &mutable_flags()->kLogToConsole);
-      Checkbox("Enable Texture Streaming",
-               &mutable_flags()->kLoadTexturesAsStreaming);
-      Checkbox("Use Bitmap Manager", &mutable_flags()->kUseBitmapManager);
-      Checkbox("Log Instructions to Debugger",
-               &mutable_flags()->kLogInstructions);
-      Checkbox("Save All Palettes", &mutable_flags()->kSaveAllPalettes);
-      Checkbox("Save Gfx Groups", &mutable_flags()->kSaveGfxGroups);
-      Checkbox("Use New ImGui Input", &mutable_flags()->kUseNewImGuiInput);
+    if (BeginMenu("Overworld Flags")) {
+      Checkbox("Enable Overworld Sprites",
+               &mutable_flags()->overworld.kDrawOverworldSprites);
+      Separator();
+      Checkbox("Save Overworld Maps",
+               &mutable_flags()->overworld.kSaveOverworldMaps);
+      Checkbox("Save Overworld Entrances",
+               &mutable_flags()->overworld.kSaveOverworldEntrances);
+      Checkbox("Save Overworld Exits",
+               &mutable_flags()->overworld.kSaveOverworldExits);
+      Checkbox("Save Overworld Items",
+               &mutable_flags()->overworld.kSaveOverworldItems);
+      Checkbox("Save Overworld Properties",
+               &mutable_flags()->overworld.kSaveOverworldProperties);
       ImGui::EndMenu();
     }
+
+    if (BeginMenu("Dungeon Flags")) {
+      Checkbox("Draw Dungeon Room Graphics",
+               &mutable_flags()->kDrawDungeonRoomGraphics);
+      Separator();
+      Checkbox("Save Dungeon Maps", &mutable_flags()->kSaveDungeonMaps);
+      ImGui::EndMenu();
+    }
+
+    if (BeginMenu("Emulator Flags")) {
+      Checkbox("Load Audio Device", &mutable_flags()->kLoadAudioDevice);
+      ImGui::EndMenu();
+    }
+
+    Checkbox("Use built-in file dialog",
+             &mutable_flags()->kNewFileDialogWrapper);
+    Checkbox("Enable Console Logging", &mutable_flags()->kLogToConsole);
+    Checkbox("Enable Texture Streaming",
+             &mutable_flags()->kLoadTexturesAsStreaming);
+    Checkbox("Use Bitmap Manager", &mutable_flags()->kUseBitmapManager);
+    Checkbox("Log Instructions to Debugger",
+             &mutable_flags()->kLogInstructions);
+    Checkbox("Save All Palettes", &mutable_flags()->kSaveAllPalettes);
+    Checkbox("Save Gfx Groups", &mutable_flags()->kSaveGfxGroups);
+    Checkbox("Use New ImGui Input", &mutable_flags()->kUseNewImGuiInput);
   }
 };
 
