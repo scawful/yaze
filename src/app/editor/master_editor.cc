daf5208559145fe8c9c7fddd0b561caf84727c64
scawful <justin.scofield@outlook.com>
2024-07-24 14:55:33 -0400

    add MasterEditor::RegisterTests

--- src/app/editor/master_editor.cc
+++ src/app/editor/master_editor.cc
@@ -9,6 +9,7 @@
 #include <imgui/misc/cpp/imgui_stdlib.h>
 #include <imgui_internal.h>
 #include <imgui_memory_editor.h>
+#include <imgui_test_engine/imgui_te_ui.h>
 
 #include "absl/status/status.h"
 #include "app/core/common.h"
@@ -421,6 +422,7 @@ void MasterEditor::DrawYazeMenu() {
     DrawFileMenu();
     DrawEditMenu();
     DrawViewMenu();
+    DrawTestMenu();
     DrawProjectMenu();
     DrawHelpMenu();
 
@@ -670,6 +672,73 @@ void MasterEditor::DrawViewMenu() {
     MenuItem("ImGui Metrics", nullptr, &show_imgui_metrics);
     EndMenu();
   }
+}
+
+void MasterEditor::RegisterTests(ImGuiTestEngine* e) {
+  test_engine = e;
+  ImGuiTest* t = nullptr;
+
+  t = IM_REGISTER_TEST(e, "master_editor", "open_rom");
+  t->GuiFunc = [](ImGuiTestContext* ctx) {
+    IM_UNUSED(ctx);
+    ImGui::Begin("Test Window", nullptr, ImGuiWindowFlags_NoSavedSettings);
+    ImGui::Text("Hello, automation world");
+    ImGui::Button("Click Me");
+    if (ImGui::TreeNode("Node")) {
+      static bool b = false;
+      ImGui::Checkbox("Checkbox", &b);
+      ImGui::TreePop();
+    }
+    ImGui::End();
+  };
+  t->TestFunc = [](ImGuiTestContext* ctx) {
+    ctx->SetRef("Test Window");
+    ctx->ItemClick("Click Me");
+    ctx->ItemOpen("Node");  // Optional as ItemCheck("Node/Checkbox") can do it
+    ctx->ItemCheck("Node/Checkbox");
+    ctx->ItemUncheck("Node/Checkbox");
+  };
+
+  t = IM_REGISTER_TEST(e, "master_editor", "use_variables");
+  struct TestVars2 {
+    int MyInt = 42;
+  };
+  t->SetVarsDataType<TestVars2>();
+  t->GuiFunc = [](ImGuiTestContext* ctx) {
+    TestVars2& vars = ctx->GetVars<TestVars2>();
+    ImGui::Begin("Test Window", nullptr, ImGuiWindowFlags_NoSavedSettings);
+    ImGui::SliderInt("Slider", &vars.MyInt, 0, 1000);
+    ImGui::End();
+  };
+  t->TestFunc = [](ImGuiTestContext* ctx) {
+    TestVars2& vars = ctx->GetVars<TestVars2>();
+    ctx->SetRef("Test Window");
+
+    IM_CHECK_EQ(vars.MyInt, 42);
+    ctx->ItemInputValue("Slider", 123);
+    IM_CHECK_EQ(vars.MyInt, 123);
+  };
+
+  t = IM_REGISTER_TEST(e, "master_editor", "open_metrics");
+  t->GuiFunc = [](ImGuiTestContext* ctx) {
+    IM_UNUSED(ctx);
+    ImGui::ShowMetricsWindow();
+  };
+  t->TestFunc = [](ImGuiTestContext* ctx) {
+    ctx->SetRef("Dear ImGui Metrics");
+  };
+}
+
+void MasterEditor::DrawTestMenu() {
+  static bool show_tests_ = false;
+
+  if (BeginMenu("Tests")) {
+    MenuItem("Run Tests", nullptr, &show_tests_);
+    EndMenu();
+  }
+
+  if (show_tests_)
+    ImGuiTestEngine_ShowTestEngineWindows(test_engine, &show_tests_);
 }
 
 void MasterEditor::DrawProjectMenu() {
