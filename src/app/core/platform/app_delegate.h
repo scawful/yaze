bf47e5ee9987eef37cd7b2f3a7586c8d13da838a
scawful <justin.scofield@outlook.com>
2024-08-02 15:34:06 -0400

    add iOS AppDelegate interface to app_delegate.h

--- src/app/core/platform/app_delegate.h
+++ src/app/core/platform/app_delegate.h
@@ -1,7 +1,26 @@
 #ifndef YAZE_APP_CORE_PLATFORM_APP_DELEGATE_H
 #define YAZE_APP_CORE_PLATFORM_APP_DELEGATE_H
 
-#ifdef TARGET_OS_MAC
+#if defined(__APPLE__) && defined(__MACH__)
+/* Apple OSX and iOS (Darwin). */
+#import <CoreText/CoreText.h>
+#include <TargetConditionals.h>
+
+#if TARGET_IPHONE_SIMULATOR == 1 || TARGET_OS_IPHONE == 1
+/* iOS in Xcode simulator */
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate, UIDocumentPickerDelegate>
+@property (strong, nonatomic) UIWindow *window;
+
+@property  UIDocumentPickerViewController *documentPicker;
+@property (nonatomic, copy) void (^completionHandler)(NSString *selectedFile);
+
+- (void)presentDocumentPickerWithCompletionHandler:(void (^)(NSString *selectedFile))completionHandler;
+
+@end
+
+#elif TARGET_OS_MAC == 1
 
 #ifdef __cplusplus
 extern "C" {
@@ -14,5 +33,7 @@ void InitializeCocoa();
 #endif
 
 #endif  // TARGET_OS_MAC
+
+#endif  // defined(__APPLE__) && defined(__MACH__)
 
 #endif  // YAZE_APP_CORE_PLATFORM_APP_DELEGATE_H
