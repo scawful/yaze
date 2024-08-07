8fa495cb17c518b14cc706289014c256cd15573c
scawful <justin.scofield@outlook.com>
2024-08-07 15:19:13 -0400

    fix font_loader.h

--- src/app/core/platform/font_loader.h
+++ src/app/core/platform/font_loader.h
@@ -2,8 +2,6 @@
 #ifndef FONTLOADER_H
 #define FONTLOADER_H
 
-#include "TargetConditionals.h"
-
 #ifdef _WIN32
 #include <Windows.h>
 // Windows specific function declaration for loading system fonts into ImGui
@@ -11,15 +9,21 @@ int CALLBACK EnumFontFamExProc(const LOGFONT* lpelfe, const TEXTMETRIC* lpntme,
                                DWORD FontType, LPARAM lParam);
 #elif __APPLE__
 
-#ifdef TARGET_OS_MAC
+#include "TargetConditionals.h"
+
+#if TARGET_OS_MAC == 1
 
 void LoadSystemFonts();
 
-#elif TARGET_OS_IPHONE
+#elif TARGET_OS_IPHONE == 1
 
 void LoadSystemFonts();
 
 #endif
+
+#elif __linux__
+
+// TODO: Implement Linux specific function declaration for loading system fonts
 
 #endif
 
