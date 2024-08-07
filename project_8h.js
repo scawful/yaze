d44661f3cc7dd510147934bf20362e17eb15b388
scawful <scawful@users.noreply.github.com>
2024-08-07 19:08:57 +0000

    deploy: 535e7eccda01c2d9b95426d6e73933cdaa8f1743

--- project_8h.js
+++ project_8h.js
@@ -1,8 +1,8 @@
 var project_8h =
 [
     [ "yaze::app::Project", "structyaze_1_1app_1_1Project.html", "structyaze_1_1app_1_1Project" ],
-    [ "kProjectFileExtension", "project_8h.html#a7f1e90bf658edf14eb7038145d8be362", null ],
+    [ "kProjectFileExtension", "project_8h.html#aaa0ddd7e20ea275692f1b4d202615dde", null ],
     [ "kProjectFileFilter", "project_8h.html#afe0922318fe6bfc7b4ea55bea180d456", null ],
-    [ "kPreviousRomFilenameDelimiter", "project_8h.html#a2fefeaeeb2e8568fc27fcf1a4b984b4d", null ],
-    [ "kEndOfProjectFile", "project_8h.html#a6448c74d503d426bcc66a4fb72789ab9", null ]
+    [ "kPreviousRomFilenameDelimiter", "project_8h.html#abcb7444c0b9ae138cd8cf01578e2f516", null ],
+    [ "kEndOfProjectFile", "project_8h.html#a53e91aa44cc7b06a75b59816a76ead79", null ]
 ];
