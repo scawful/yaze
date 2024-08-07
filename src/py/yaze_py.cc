bf2824e9dc3a5381a45a1907f10b57005571c331
scawful <justin.scofield@outlook.com>
2024-08-07 11:28:14 -0400

    add snes_color, snes_palette, sprite_action, sprite to yaze_py module

--- src/py/yaze_py.cc
+++ src/py/yaze_py.cc
@@ -18,6 +18,26 @@ BOOST_PYTHON_MODULE(yaze) {
       .def_readonly("size", &Rom::size)
       .def_readonly("impl", &Rom::impl);
 
+  class_<snes_color>("snes_color")
+      .def_readonly("red", &snes_color::red)
+      .def_readonly("green", &snes_color::green)
+      .def_readonly("blue", &snes_color::blue);
+
+  class_<snes_palette>("snes_palette")
+      .def_readonly("id", &snes_palette::id)
+      .def_readonly("size", &snes_palette::size)
+      .def_readonly("colors", &snes_palette::colors);
+
+  class_<sprite_action>("sprite_action")
+      .def_readonly("name", &sprite_action::name)
+      .def_readonly("id", &sprite_action::id);
+
+  class_<sprite>("sprite")
+      .def_readonly("name", &sprite::name)
+      .def_readonly("id", &sprite::id)
+      .def_readonly("actions", &sprite::actions);
+}
+
 /**
  * Python C API Example, in case I need more functionality than Boost.Python
  */
