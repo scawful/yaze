13995d40bf336bd01223a13fe81b3c26dda26d5f
Justin Scofield <justin.scofield@outlook.com>
2024-08-08 18:02:31 -0400

    cleanup clipboard.h

--- src/app/core/platform/clipboard.h
+++ src/app/core/platform/clipboard.h
@@ -1,27 +1,10 @@
 #ifndef YAZE_APP_CORE_PLATFORM_CLIPBOARD_H
 #define YAZE_APP_CORE_PLATFORM_CLIPBOARD_H
 
-#ifdef _WIN32
-
-void CopyImageToClipboard(const std::vector<uint8_t>& data);
-void GetImageFromClipboard(std::vector<uint8_t>& data, int& width, int& height);
-
-#elif defined(__APPLE__)
-
 #include <cstdint>
 #include <vector>
 
-void CopyImageToClipboard(const std::vector<uint8_t>& data);
-void GetImageFromClipboard(std::vector<uint8_t>& data, int& width, int& height);
+void CopyImageToClipboard(const std::vector<uint8_t> &data);
+void GetImageFromClipboard(std::vector<uint8_t> &data, int &width, int &height);
 
-#elif defined(__linux__)
-
-#include <cstdint>
-#include <vector>
-
-void CopyImageToClipboard(const std::vector<uint8_t>& data);
-void GetImageFromClipboard(std::vector<uint8_t>& data, int& width, int& height);
-
-#endif
-
-#endif  // YAZE_APP_CORE_PLATFORM_CLIPBOARD_H
+#endif // YAZE_APP_CORE_PLATFORM_CLIPBOARD_H
