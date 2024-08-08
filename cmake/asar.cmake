7379a8535c8dedbf6b24f300d7a1642bd892421f
scawful <justin.scofield@outlook.com>
2024-08-08 13:42:32 -0400

    cmakelists build cleanup

--- cmake/asar.cmake
+++ cmake/asar.cmake
@@ -1,33 +1,36 @@
-get_target_property(ASAR_INCLUDE_DIR asar-static INCLUDE_DIRECTORIES)
-target_include_directories(asar-static PRIVATE ${ASAR_INCLUDE_DIR})
+# Asar Assembler for 65816 SNES Assembly
 set(ASAR_GEN_EXE OFF)
 set(ASAR_GEN_DLL ON)
 set(ASAR_GEN_LIB ON)
 set(ASAR_GEN_EXE_TEST OFF)
 set(ASAR_GEN_DLL_TEST OFF)
+set(ASAR_STATIC_SRC_DIR "${CMAKE_SOURCE_DIR}/src/lib/asar/src/asar")
+
+get_target_property(ASAR_INCLUDE_DIR asar-static INCLUDE_DIRECTORIES)
+target_include_directories(asar-static PRIVATE ${ASAR_INCLUDE_DIR})
 
 set(ASAR_STATIC_SRC
-  "${CMAKE_SOURCE_DIR}/src/lib/asar/src/asar/interface-lib.cpp"
-  "${CMAKE_SOURCE_DIR}/src/lib/asar/src/asar/addr2line.cpp"
-  "${CMAKE_SOURCE_DIR}/src/lib/asar/src/asar/arch-65816.cpp"
-  "${CMAKE_SOURCE_DIR}/src/lib/asar/src/asar/arch-spc700.cpp"
-  "${CMAKE_SOURCE_DIR}/src/lib/asar/src/asar/arch-superfx.cpp"
-  "${CMAKE_SOURCE_DIR}/src/lib/asar/src/asar/assembleblock.cpp"
-  "${CMAKE_SOURCE_DIR}/src/lib/asar/src/asar/crc32.cpp"
-  "${CMAKE_SOURCE_DIR}/src/lib/asar/src/asar/libcon.cpp"
-  "${CMAKE_SOURCE_DIR}/src/lib/asar/src/asar/libsmw.cpp"
-  "${CMAKE_SOURCE_DIR}/src/lib/asar/src/asar/libstr.cpp"
-  "${CMAKE_SOURCE_DIR}/src/lib/asar/src/asar/macro.cpp"
-  "${CMAKE_SOURCE_DIR}/src/lib/asar/src/asar/main.cpp"
-  "${CMAKE_SOURCE_DIR}/src/lib/asar/src/asar/asar_math.cpp"
-  "${CMAKE_SOURCE_DIR}/src/lib/asar/src/asar/virtualfile.cpp"
-  "${CMAKE_SOURCE_DIR}/src/lib/asar/src/asar/warnings.cpp"
-  "${CMAKE_SOURCE_DIR}/src/lib/asar/src/asar/errors.cpp"
-  "${CMAKE_SOURCE_DIR}/src/lib/asar/src/asar/platform/file-helpers.cpp"
+  "${ASAR_STATIC_SRC_DIR}/interface-lib.cpp"
+  "${ASAR_STATIC_SRC_DIR}/addr2line.cpp"
+  "${ASAR_STATIC_SRC_DIR}/arch-65816.cpp"
+  "${ASAR_STATIC_SRC_DIR}/arch-spc700.cpp"
+  "${ASAR_STATIC_SRC_DIR}/arch-superfx.cpp"
+  "${ASAR_STATIC_SRC_DIR}/assembleblock.cpp"
+  "${ASAR_STATIC_SRC_DIR}/crc32.cpp"
+  "${ASAR_STATIC_SRC_DIR}/libcon.cpp"
+  "${ASAR_STATIC_SRC_DIR}/libsmw.cpp"
+  "${ASAR_STATIC_SRC_DIR}/libstr.cpp"
+  "${ASAR_STATIC_SRC_DIR}/macro.cpp"
+  "${ASAR_STATIC_SRC_DIR}/main.cpp"
+  "${ASAR_STATIC_SRC_DIR}/asar_math.cpp"
+  "${ASAR_STATIC_SRC_DIR}/virtualfile.cpp"
+  "${ASAR_STATIC_SRC_DIR}/warnings.cpp"
+  "${ASAR_STATIC_SRC_DIR}/errors.cpp"
+  "${ASAR_STATIC_SRC_DIR}/platform/file-helpers.cpp"
 )
 
 if(WIN32 OR MINGW)
-  list(APPEND ASAR_STATIC_SRC "${CMAKE_SOURCE_DIR}/src/lib/asar/src/asar/platform/windows/file-helpers-win32.cpp")
+  list(APPEND ASAR_STATIC_SRC "${ASAR_STATIC_SRC_DIR}/platform/windows/file-helpers-win32.cpp")
 else()
-  list(APPEND ASAR_STATIC_SRC "${CMAKE_SOURCE_DIR}/src/lib/asar/src/asar/platform/linux/file-helpers-linux.cpp")
+  list(APPEND ASAR_STATIC_SRC "${ASAR_STATIC_SRC_DIR}/platform/linux/file-helpers-linux.cpp")
 endif()
