59d837dab58e0f406e5578c9159ee02b4ff855f1
scawful <justin.scofield@outlook.com>
2024-08-11 13:14:23 -0400

    add integration arg to yaze_test for test_engine window

--- src/test/yaze_test.cc
+++ src/test/yaze_test.cc
@@ -1,4 +1,6 @@
 #define SDL_MAIN_HANDLED
+
+#include <SDL.h>
 #include <gtest/gtest.h>
 
 #include "absl/debugging/failure_signal_handler.h"
@@ -9,6 +11,16 @@ int main(int argc, char* argv[]) {
 
   absl::FailureSignalHandlerOptions options;
   absl::InstallFailureSignalHandler(options);
+
+  // Support the ability to launch an integration test window.
+  SDL_SetMainReady();
+
+  // Check if the argument says `integration`
+  if (argc > 1 && std::string(argv[1]) == "integration") {
+    // TODO: Create the debugging window with the test engine.
+    return EXIT_SUCCESS;
+  }
+
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
