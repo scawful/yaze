83c298b7972ef149b22c2153b1722a66a356e4a1
scawful <justin.scofield@outlook.com>
2024-08-13 20:49:31 -0400

    chore: include imgui_test_engine

---
+++ src/test/integration/test_editor.cc
@@ -0,0 +1,39 @@
+#include "test/integration/test_editor.h"
+
+#include "imgui/imgui.h"
+#include "imgui_test_engine/imgui_te_context.h"
+#include "imgui_test_engine/imgui_te_engine.h"
+#include "imgui_test_engine/imgui_te_ui.h"
+
+namespace yaze_test {
+namespace integration {
+
+absl::Status TestEditor::Update() {
+  if (ImGui::Begin("My Window")) {
+    ImGui::Text("Hello, world!");
+    ImGui::Button("My Button");
+  }
+
+  static bool show_demo_window = true;
+
+  ImGuiTestEngine_ShowTestEngineWindows(engine_, &show_demo_window);
+
+  ImGui::End();
+
+  return absl::OkStatus();
+}
+
+void TestEditor::RegisterTests(ImGuiTestEngine* engine) {
+  ImGuiTest* test = IM_REGISTER_TEST(engine, "demo_test", "test1");
+  test->TestFunc = [](ImGuiTestContext* ctx) {
+    ctx->SetRef("My Window");
+    ctx->ItemClick("My Button");
+    ctx->ItemCheck("Node/Checkbox");
+    ctx->ItemInputValue("Slider", 123);
+
+    ctx->MenuCheck("//Dear ImGui Demo/Tools/About Dear ImGui");
+  };
+}
+
+}  // namespace integration
+}  // namespace yaze_test
