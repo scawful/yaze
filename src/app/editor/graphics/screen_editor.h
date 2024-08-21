4e908d0f2625f9174d8c3600c02e991d3c65be33
scawful <justin.scofield@outlook.com>
2024-08-21 00:12:21 -0400

    chore: Refactor screen editor to improve code organization and remove unnecessary includes

--- src/app/editor/graphics/screen_editor.h
+++ src/app/editor/graphics/screen_editor.h
@@ -1,8 +1,6 @@
 #ifndef YAZE_APP_EDITOR_SCREEN_EDITOR_H
 #define YAZE_APP_EDITOR_SCREEN_EDITOR_H
 
-#include "imgui/imgui.h"
-
 #include <array>
 
 #include "absl/status/status.h"
@@ -18,6 +16,7 @@
 #include "app/rom.h"
 #include "app/zelda3/screen/dungeon_map.h"
 #include "app/zelda3/screen/inventory.h"
+#include "imgui/imgui.h"
 
 namespace yaze {
 namespace app {
@@ -69,34 +68,34 @@ class ScreenEditor : public SharedRom, public Editor {
   void DrawDungeonMapsTabs();
   void DrawDungeonMapsEditor();
 
-  std::vector<zelda3::screen::DungeonMap> dungeon_maps_;
-  std::vector<std::vector<std::array<std::string, 25>>> dungeon_map_labels_;
-
-  std::unordered_map<int, gfx::Bitmap> tile16_individual_;
-
   bool dungeon_maps_loaded_ = false;
+
+  uint8_t selected_room = 0;
+  uint8_t boss_room = 0;
 
   int selected_tile16_ = 0;
   int selected_dungeon = 0;
-  uint8_t selected_room = 0;
-  uint8_t boss_room = 0;
   int floor_number = 1;
 
   bool copy_button_pressed = false;
   bool paste_button_pressed = false;
 
   std::vector<uint8_t> all_gfx_;
-  zelda3::screen::Inventory inventory_;
+  std::unordered_map<int, gfx::Bitmap> tile16_individual_;
+  std::vector<zelda3::screen::DungeonMap> dungeon_maps_;
+  std::vector<std::vector<std::array<std::string, 25>>> dungeon_map_labels_;
+
+  absl::Status status_;
+
   gfx::SnesPalette palette_;
+  gfx::BitmapTable sheets_;
+  gfx::Tilesheet tile16_sheet_;
+
   gui::Canvas screen_canvas_;
   gui::Canvas tilesheet_canvas_;
   gui::Canvas tilemap_canvas_;
 
-  gfx::BitmapTable sheets_;
-
-  gfx::Tilesheet tile16_sheet_;
-
-  absl::Status status_;
+  zelda3::screen::Inventory inventory_;
 };
 
 }  // namespace editor
