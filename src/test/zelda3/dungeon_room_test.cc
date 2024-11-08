14f4c390b2666e93eeebc825a8ed04437fe7a54f
scawful <justin.scofield@outlook.com>
2024-11-08 00:18:33 -0500

    Refactor dungeon room initialization for clarity and add unit tests for room loading functionality

---
+++ src/test/zelda3/dungeon_room_test.cc
@@ -0,0 +1,37 @@
+#include "app/zelda3/dungeon/room.h"
+
+#include "gtest/gtest.h"
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+#include "app/rom.h"
+
+namespace yaze {
+namespace test {
+namespace zelda3 {
+
+class DungeonRoomTest : public ::testing::Test, public app::SharedRom {
+protected:
+  void SetUp() override {
+    // Skip tests on Linux for automated github builds
+#if defined(__linux__)
+    GTEST_SKIP();
+#else
+    if (!rom()->LoadFromFile("./zelda3.sfc").ok()) {
+      GTEST_SKIP_("Failed to load test ROM");
+    }
+#endif
+  }
+  void TearDown() override {}
+};
+
+TEST_F(DungeonRoomTest, SingleRoomLoadOk) {
+  app::zelda3::dungeon::Room test_room(/*room_id=*/0);
+  test_room.LoadHeader();
+  // Do some assertions based on the output in ZS
+  test_room.LoadRoomFromROM();
+}
+
+} // namespace zelda3
+} // namespace test
+} // namespace yaze
