0adfdfb135cc46331a886d827f7971ecf2142c10
scawful <justin.scofield@outlook.com>
2024-12-21 18:56:47 -0500

    Add getter for active state in Controller class

--- src/app/core/controller.h
+++ src/app/core/controller.h
@@ -54,6 +54,7 @@ class Controller : public ExperimentFlags {
   auto window() -> SDL_Window * { return window_.get(); }
   void init_test_editor(editor::Editor *editor) { test_editor_ = editor; }
   void set_active(bool active) { active_ = active; }
+  auto active() { return active_; }
 
  private:
   friend int ::main(int argc, char **argv);
