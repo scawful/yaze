2d7c8d69414408e645bb3a6112a2db8f209d250a
scawful <justin.scofield@outlook.com>
2024-12-29 10:44:22 -0500

    Refactor file loading functions: simplify LoadFile, add LoadConfigFile, and adjust platform handling

--- src/app/core/utils/file_util.cc
+++ src/app/core/utils/file_util.cc
@@ -29,8 +29,31 @@ std::string GetFileName(const std::string &filename) {
   return filename.substr(slash + 1);
 }
 
-std::string LoadFile(const std::string &filename, Platform platform) {
+std::string LoadFile(const std::string &filename) {
   std::string contents;
+  std::ifstream file(filename);
+  if (file.is_open()) {
+    std::stringstream buffer;
+    buffer << file.rdbuf();
+    contents = buffer.str();
+    file.close();
+  } else {
+    // Throw an exception
+    throw std::runtime_error("Could not open file: " + filename);
+  }
+  return contents;
+}
+
+std::string LoadConfigFile(const std::string &filename) {
+  std::string contents;
+  Platform platform;
+#if defined(_WIN32)
+  platform = Platform::kWindows;
+#elif defined(__APPLE__)
+  platform = Platform::kMacOS;
+#else
+  platform = Platform::kLinux;
+#endif
   std::string filepath = GetConfigDirectory(platform) + "/" + filename;
   std::ifstream file(filepath);
   if (file.is_open()) {
@@ -55,18 +78,18 @@ void SaveFile(const std::string &filename, const std::string &contents,
 std::string GetConfigDirectory(Platform platform) {
   std::string config_directory = ".yaze";
   switch (platform) {
-    case Platform::kWindows:
-      config_directory = "~/AppData/Roaming/yaze";
-      break;
-    case Platform::kMacOS:
-    case Platform::kLinux:
-      config_directory = "~/.config/yaze";
-      break;
-    default:
-      break;
+  case Platform::kWindows:
+    config_directory = "~/AppData/Roaming/yaze";
+    break;
+  case Platform::kMacOS:
+  case Platform::kLinux:
+    config_directory = "~/.config/yaze";
+    break;
+  default:
+    break;
   }
   return config_directory;
 }
 
-}  // namespace core
-}  // namespace yaze
+} // namespace core
+} // namespace yaze
