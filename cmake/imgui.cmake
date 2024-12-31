5ec8d607f2930c5176b35a670cff754fb908ce14
scawful <justin.scofield@outlook.com>
2024-12-31 15:49:22 -0500

    Remove ImGuiFileDialog integration from CMake configuration

--- cmake/imgui.cmake
+++ cmake/imgui.cmake
@@ -7,11 +7,6 @@ target_include_directories(ImGui PUBLIC ${SDL2_INCLUDE_DIR})
 target_compile_definitions(ImGui PUBLIC
   IMGUI_IMPL_OPENGL_LOADER_CUSTOM=<SDL2/SDL_opengl.h>  GL_GLEXT_PROTOTYPES=1)
 
-set(IMGUI_FILE_DLG_PATH ${CMAKE_SOURCE_DIR}/src/lib/ImGuiFileDialog)
-file(GLOB IMGUI_FILE_DLG_SOURCES ${IMGUI_FILE_DLG_PATH}/*.cpp)
-add_library("ImGuiFileDialog" STATIC ${IMGUI_FILE_DLG_SOURCES})
-target_include_directories(ImGuiFileDialog PUBLIC ${IMGUI_PATH}  ${CMAKE_SOURCE_DIR}/src/lib)
-
 set(IMGUI_TEST_ENGINE_PATH ${CMAKE_SOURCE_DIR}/src/lib/imgui_test_engine/imgui_test_engine)
 file(GLOB IMGUI_TEST_ENGINE_SOURCES ${IMGUI_TEST_ENGINE_PATH}/*.cpp)
 add_library("ImGuiTestEngine" STATIC ${IMGUI_TEST_ENGINE_SOURCES})
@@ -27,7 +22,6 @@ set(
   ${IMGUI_PATH}/backends/imgui_impl_sdl2.cpp
   ${IMGUI_PATH}/backends/imgui_impl_sdlrenderer2.cpp
   ${IMGUI_PATH}/misc/cpp/imgui_stdlib.cpp
-  ${IMGUI_FILE_DLG_PATH}/ImGuiFileDialog.cpp
 )
 
 # For integration test
