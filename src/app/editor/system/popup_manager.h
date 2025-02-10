c66541db87b5d6e41a44a8f02d1e802ed678478b
scawful <justin.scofield@outlook.com>
2025-02-10 11:51:03 -0500

    Add PopupManager: integrate PopupManager class and PopupParams structure for enhanced popup handling

--- src/app/editor/system/popup_manager.h
+++ src/app/editor/system/popup_manager.h
@@ -1,19 +1,26 @@
 #ifndef YAZE_APP_EDITOR_POPUP_MANAGER_H
 #define YAZE_APP_EDITOR_POPUP_MANAGER_H
 
+#include <string>
+
 namespace yaze {
 namespace editor {
+
+struct PopupParams {
+  std::string name;
+};
 
 // ImGui popup manager.
 class PopupManager {
  public:
-  PopupManager();
-  ~PopupManager();
 
   void Show(const char* name);
+
+ private:
+  std::vector<PopupParams> popups_;
 };
 
-} // namespace editor
-} // namespace yaze
+}  // namespace editor
+}  // namespace yaze
 
-#endif // YAZE_APP_EDITOR_POPUP_MANAGER_H
+#endif  // YAZE_APP_EDITOR_POPUP_MANAGER_H
