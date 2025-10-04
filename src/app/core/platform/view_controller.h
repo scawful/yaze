c39b1e5a51addc230c627fcaee1a01a0901596bf
scawful <justin.scofield@outlook.com>
2025-10-04 13:48:25 -0400

    Update scheme management plist to adjust order hints for iOS and macOS schemes
    - Changed orderHint for yaze_ios.xcscheme from 12 to 10
    - Changed orderHint for yaze_macos.xcscheme from 5 to 11

--- src/app/core/platform/view_controller.h
+++ src/app/core/platform/view_controller.h
@@ -5,10 +5,13 @@
 #include <TargetConditionals.h>
 
 #if TARGET_OS_OSX
-#include "imgui_impl_osx.h"
+#ifdef __OBJC__
 @interface AppViewController : NSViewController <NSWindowDelegate>
+@property(nonatomic) yaze::core::Controller *controller;
 @end
+#endif
 #else
+#ifdef __OBJC__
 @interface AppViewController : UIViewController <MTKViewDelegate>
 @property(nonatomic) yaze::core::Controller *controller;
 @property(nonatomic) UIHoverGestureRecognizer *hoverGestureRecognizer;
@@ -17,13 +20,16 @@
 @property(nonatomic) UILongPressGestureRecognizer *longPressRecognizer;
 @end
 #endif
+#endif
 
+#ifdef __OBJC__
 @interface AppViewController () <MTKViewDelegate>
 @property(nonatomic, readonly) MTKView *mtkView;
 @property(nonatomic, strong) id<MTLDevice> device;
 @property(nonatomic, strong) id<MTLCommandQueue> commandQueue;
 @end
+#endif
 
 #endif  // __APPLE__
 
-#endif  // YAZE_APP_CORE_PLATFORM_APP_VIEW_CONTROLLER_H
+#endif  // YAZE_APP_CORE_PLATFORM_VIEW_CONTROLLER_H
