9dbd2a6ab3013dbaa6f15052010237973a2bbb56
scawful <justin.scofield@outlook.com>
2026-04-19 18:03:27 -0400

    editor: restore legacy editor panel alias shim

--- src/app/editor/system/editor_panel.h
+++ src/app/editor/system/editor_panel.h
@@ -1,4 +1,14 @@
-#ifndef YAZE_APP_EDITOR_SYSTEM_EDITOR_PANEL_H_
+#ifndef YAZE_APP_EDITOR_SYSTEM_EDITOR_PANEL_LEGACY_PATH_H_
+#define YAZE_APP_EDITOR_SYSTEM_EDITOR_PANEL_LEGACY_PATH_H_
+
 #include "app/editor/system/workspace/editor_panel.h"
-#define YAZE_APP_EDITOR_SYSTEM_EDITOR_PANEL_H_
-#endif  // YAZE_APP_EDITOR_SYSTEM_EDITOR_PANEL_H_
+
+namespace yaze {
+namespace editor {
+
+using EditorPanel = WindowContent;
+
+}  // namespace editor
+}  // namespace yaze
+
+#endif  // YAZE_APP_EDITOR_SYSTEM_EDITOR_PANEL_LEGACY_PATH_H_
