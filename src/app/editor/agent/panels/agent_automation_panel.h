e1c4db8717fc8212c29287e2fc70c18719990b0e
scawful <justin.scofield@outlook.com>
2026-04-19 18:25:54 -0400

    editor: collapse legacy panel headers into shims

--- src/app/editor/agent/panels/agent_automation_panel.h
+++ src/app/editor/agent/panels/agent_automation_panel.h
@@ -1,22 +1,4 @@
 #ifndef YAZE_APP_EDITOR_AGENT_PANELS_AGENT_AUTOMATION_PANEL_H_
+#include "app/editor/agent/ui/window/agent_automation_panel.h"
 #define YAZE_APP_EDITOR_AGENT_PANELS_AGENT_AUTOMATION_PANEL_H_
-
-#include <functional>
-#include <string>
-
-#include "app/editor/agent/agent_state.h"
-
-namespace yaze {
-namespace editor {
-
-class AgentAutomationPanel {
- public:
-  AgentAutomationPanel() = default;
-
-  void Draw(AgentUIContext* context, const AutomationCallbacks& callbacks);
-};
-
-}  // namespace editor
-}  // namespace yaze
-
 #endif  // YAZE_APP_EDITOR_AGENT_PANELS_AGENT_AUTOMATION_PANEL_H_
--- src/app/editor/agent/panels/agent_z3ed_command_panel.h
+++ src/app/editor/agent/panels/agent_z3ed_command_panel.h
@@ -1,24 +1,4 @@
 #ifndef YAZE_APP_EDITOR_AGENT_PANELS_AGENT_Z3ED_COMMAND_PANEL_H_
+#include "app/editor/agent/ui/window/agent_z3ed_command_panel.h"
 #define YAZE_APP_EDITOR_AGENT_PANELS_AGENT_Z3ED_COMMAND_PANEL_H_
-
-#include <functional>
-#include <string>
-
-#include "app/editor/agent/agent_state.h"
-
-namespace yaze {
-namespace editor {
-
-class ToastManager;
-
-class AgentZ3EDCommandPanel {
- public:
-  AgentZ3EDCommandPanel() = default;
-
-  void Draw(AgentUIContext* context, const Z3EDCommandCallbacks& callbacks, ToastManager* toast_manager);
-};
-
-}  // namespace editor
-}  // namespace yaze
-
 #endif  // YAZE_APP_EDITOR_AGENT_PANELS_AGENT_Z3ED_COMMAND_PANEL_H_
