36f20db6aee0825ee0159a4f5849119bacec24ac
scawful <justin.scofield@outlook.com>
2026-04-19 22:30:16 -0400

    editor: retire legacy ui feedback sources

--- src/app/editor/ui/about_panel.cc
+++
@@ -1,9 +0,0 @@
-#include "app/editor/ui/about_panel.h"
-#include "app/editor/core/panel_registration.h"
-
-namespace yaze::editor {
-
-// Auto-register AboutPanel with ContentRegistry
-REGISTER_PANEL(AboutPanel);
-
-}  // namespace yaze::editor
