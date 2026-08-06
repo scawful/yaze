faef8da156d6f5c372744cdfa3561c0de5b3c196
scawful <justin.scofield@outlook.com>
2026-08-06 12:48:45 -0400

    test(ios): verify exported bundle checksums

--- src/ios/iOS/YazeRomChecksum.swift
+++ src/ios/iOS/YazeRomChecksum.swift
@@ -7,4 +7,8 @@ enum YazeRomChecksum {
       .map { String(format: "%02x", $0) }
       .joined()
   }
+
+  static func sha1Hex(contentsOf url: URL) throws -> String {
+    sha1Hex(for: try Data(contentsOf: url, options: .mappedIfSafe))
+  }
 }
